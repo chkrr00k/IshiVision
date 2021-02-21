f701f4d644ca13db4dbe7817027aba5a0d6ee5c0
chkrr00k <chkrr00k@cock.li>
2021-02-21 21:09:57 +0100

    added region joining

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import time
 import math
 
 import extract
-from utils import straight
+import utils
 import maxima
 
 #dilate = False
 #close = False
 
 #img = cv2.imread("plate4.jpg") #16 7
-img = cv2.imread("../ref/Plate12.jpg") #16 7 3 12 4 5 6 2 10
+img = cv2.imread("../ref/Plate16.jpg") #16 7 3 12 4 5 6 2 10
 #wight are approximated                        ^^
 img = cv2.bilateralFilter(img, 9, 125, 50)
 print("Image of shape {}".format(img.shape))
@@ -28,7 +28,7 @@ print("Masks calculated in: {:.3f}s".format(t1-t0))
 for n, m in ms.items():
 #    cv2.imshow("Masks dil for {}".format(n), m)
     r, c = m.shape
-    mc = straight(m)
+    mc = utils.straight(m)
     bndu, bndl = (r//8, c//7), (r*7//8, c*6//7)
     mc = mc[bndu[0]:bndl[0], bndu[1]:bndl[1]]
     _, con, hi = cv2.findContours(mc, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -40,59 +40,17 @@ for n, m in ms.items():
     for co, [_, _, _, Pa] in zip(con, hi[0]):
         re = [x, y, w, h] = cv2.boundingRect(co)
         if Pa < 0:
+            cv2.rectangle(mc, (x, y), (x+w, y+h), (0, 0, 255), 1)
             rects.append(re)
-            cv2.rectangle(mc, (x, y), (x+w, y+h), (255, 0, 0), 1)
+    for [x, y, xx, yy] in utils.reduce_sections(rects):
+        cv2.rectangle(mc, (x, y), (xx, yy), (255, 0, 0), 1)
 
-#    print(rects)
     cv2.rectangle(mc, (mc.shape[0]//4, 0), (mc.shape[0]*3//4, mc.shape[1]), (0, 255, 0), 1)
     cv2.imshow("c:{}".format(n), mc)
-
-#####################TRASH#############
-#    dil = rot(m, 15)
-#    e = cv2.Canny(dil, 100, 200)
-#    cv2.imshow("Canny 4 {}".format(n), e)
-#    dil = m
-#
-#    if dilate:
-#        el = cv2.getStructuringElement(cv2.MORPH_RECT, (int(dil_factor), int(dil_factor)))
-#        dil = cv2.dilate(dil, el)
-#    if close:
-#        ker = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(dil_factor*2), int(dil_factor*2)))
-#        dil = cv2.morphologyEx(dil, cv2.MORPH_CLOSE, ker)
-#
-
-    #~dil
- #   _, con, hi = cv2.findContours(~dil, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#    p = None
-#    for c, [_, _, _, Pa] in zip(con, hi[0]):
-
-#        if Pa == -1 and (img.shape[0]*img.shape[1])/12 < cv2.contourArea(c):
-#            [x, y, w, h] = cv2.boundingRect(c)
-#            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 1)
-#            roi = dil[y:y+h, x:x+w].copy()
- #           roi = cv2.cvtColor(roi, cv2.COLOR_GRAY2BGR)
-
-            #inverts the image if it's white dominance
-#            f = np.bincount(roi.ravel()//255)
-#            if f[0] < f[1]:
-#                roi = ~roi
-#                cv2.putText(roi, "INV", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-#            result.append(roi)
- #           p = roi
- #       elif p is not None:
- #           [x, y, w, h] = cv2.boundingRect(c)
- #           cv2.rectangle(p, (x, y), (x+w, y+h), (255, 0, 0), 1)
-
 
 
 
 cv2.imshow("Con", img)
-#print("Found {} feasible regions".format(len(result)))
 
-#for i in range(len(result)):
-#    cv2.imshow("#{}".format(i), result[i])
-
-    #cv2.imshow("C {}".format(i), e)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import math
+from functools import reduce
 
 def straight(img):
     r, c = img.shape[:2]
@@ -13,3 +14,72 @@ def rot(img, ang):
     ic = tuple(np.array(img.shape[1::-1])/2)
     rm = cv2.getRotationMatrix2D(ic, ang, 1.0)
     return cv2.warpAffine(img, rm, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+
+def rect_intersect(r1, r2, convert=False):
+    if convert:
+        r1 = rect_convert(r1)
+        r2 = rect_convert(r2)
+    return not (r1[0] > r2[2] or r1[2] < r2[0] or r1[1] > r2[3] or r1[3] < r2[1])
+
+def rect_fuse(r1, r2, convert=False):
+    if convert:
+        r1 = rect_convert(r1)
+        r2 = rect_convert(r2)
+    return (min(r1[0], r2[0]), min(r1[1], r2[1]), max(r1[2], r2[2]), max(r1[3], r2[3]))
+
+def rect_convert(input):
+    return (input[0], input[1], input[0]+input[2], input[1]+input[3])
+
+def rect_deconvert(input):
+    return (input[0], input[1], input[2]-input[0], input[3]-input[1])
+
+def reduce_sections_total(rois, convert=True):
+    if convert:
+        rois = [rect_convert(r) for r in rois]
+    i = 0
+    while i < len(rois):
+        j = 0
+        while j < len(rois):
+            if i != j and rect_intersect(rois[i], rois[j]):
+                n = rect_fuse(rois[i], rois[j])
+                rois[i] = n
+                print("Fused {}&{}=>{}".format(rois[i], rois[j], n))
+                rois.pop(j)
+                if i > j:
+                    i -= 1
+            else:
+                j += 1
+        i += 1
+    return rois
+def reduce_sections(rois, convert=True):
+    if convert:
+        rois = [rect_convert(r) for r in rois]
+    result = [[rois[0]]]
+    for c in rois[1:]:
+        for r in result:
+            if any(rect_intersect(c, rr) for rr in r):
+                r.append(c)
+                break
+        else:
+            result.append([c])
+    return [reduce(rect_fuse, r) for r in result]
+
+
+print(reduce_sections([[0,0, 1,1], [1,1, 2,2], [4,4, 5,5], [0,0,2,4], [2,2,3,3]]))
+
+a = [0,0, 1,1]
+b = [2,2, 3,3]
+print("{} (False)".format(rect_intersect(a, b)))
+print("{} (False)".format(rect_intersect(b, a)))
+
+a = [0,0, 4,4]
+b = [1,1, 2,2]
+c = [3,3, 5,5]
+print("{} (True)".format(rect_intersect(a, b)))
+print("{} (True)".format(rect_intersect(a, c)))
+print("{} (True)".format(rect_intersect(b, a)))
+print("{} (True)".format(rect_intersect(c, a)))
+
+a = [0,0, 4,4]
+b = [0,5, 5,5]
+print("{} (False)".format(rect_intersect(a, b)))
