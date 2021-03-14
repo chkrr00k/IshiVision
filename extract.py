0e9c49a3f5176b390076efb732acf08bd2af0554
chkrr00k <chkrr00k@cock.li>
2021-03-14 13:54:12 +0100

    Moved selection of optimal mask to extract

--- extract.py
+++ extract.py
@@ -6,6 +6,8 @@ import maxima
 import categorize as cat
 import visual
 import common
+import utils
+import selector
 
 @common.showtime
 def get_masks(img, remove_white=True):
@@ -45,4 +47,44 @@ def get_masks(img, remove_white=True):
 
     return result
 
+@common.showtime
+def get_optimal_mask(img, verbose=False, stra=False, write=False, show=False):
+    #weight are approximated
+    img = cv2.bilateralFilter(img, 9, 125, 50)
+    if verbose:
+        print("Image of shape {}".format(img.shape))
+
+    ms = get_masks(img)
+
+    scores = list()
+
+    for n, m in ms.items():
+        #straigten the chars (ocr step 1) not needed
+        if stra:
+            m = utils.straight(m)
+
+        #finds the contourns
+        _, con, hi = cv2.findContours(m, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if write:
+            cv2.imwrite("ref/{}.jpg".format(n), m)
+        
+        #puts all the rectangles in a list
+        rects, cont = utils.get_parent_contours(con, hi)
+
+        bounds = (m.shape[0]//5, m.shape[1]//8, m.shape[0]*4//5, m.shape[1]*7//8)
+        sc = selector.get_score(m, cont, bounds)
+        if verbose:
+            print(selector.get_score_string(*sc, n=n))
+        scores.append(sc)
+        
+        if show:
+            #compress the rectangles that are superimposed to eachothers to have only the main ones
+            cv2.imshow("{} Mask with main contourns".format(n), visual.print_contours_bounding_rect(m, utils.reduce_sections(rects), bounds))
+
+    i = selector.rank(scores, verbose=verbose)
+    if verbose:
+        print("Best fit found: {}".format(i))
+    n, best_fit = list(ms.items())[i]
+    return best_fit, n
 
--- test.py
+++ test.py
@@ -6,9 +6,6 @@ import numpy as np
 import random
 
 import extract
-import utils
-import selector
-import visual
 import generator
 
 gen = True
@@ -25,40 +22,8 @@ else:
 
 cv2.imshow("Base image", img)
 
-#weight are approximated
-img = cv2.bilateralFilter(img, 9, 125, 50)
-print("Image of shape {}".format(img.shape))
+best_fit, n = extract.get_optimal_mask(img)
 
-ms = extract.get_masks(img)
-
-scores = list()
-
-for n, m in ms.items():
-    #straigten the chars (ocr step 1) not needed
-    if stra:
-        m = utils.straight(m)
-
-    #finds the contourns
-    _, con, hi = cv2.findContours(m, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    if write:
-        cv2.imwrite("ref/{}.jpg".format(n), m)
-    
-    #puts all the rectangles in a list
-    rects, cont = utils.get_parent_contours(con, hi)
-
-    bounds = (m.shape[0]//5, m.shape[1]//8, m.shape[0]*4//5, m.shape[1]*7//8)
-    sc = selector.get_score(m, cont, bounds)
-    print(selector.get_score_string(*sc, n=n))
-    scores.append(sc)
-    
-    if show:
-        #compress the rectangles that are superimposed to eachothers to have only the main ones
-        cv2.imshow("{} Mask with main contourns".format(n), visual.print_contours_bounding_rect(m, utils.reduce_sections(rects), bounds))
-
-i = selector.rank(scores, verbose=True)
-print("Best fit found: {}".format(i))
-n, best_fit = list(ms.items())[i]
 if show:
     cv2.imshow("Best fit: {}".format(n), best_fit)
 
