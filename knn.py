b339a7085401414542194d49b8d19a64ab30f1fe
chkrr00k <chkrr00k@cock.li>
2021-03-21 12:31:44 +0100

    Created class structure
    added asserts

--- knn.py
+++ knn.py
@@ -4,103 +4,104 @@ import cv2
 import numpy as np
 
 import random
+from functools import reduce
 
-import generator
-import extract
+import ocr
 
-GLYPHS = "1234567890"
+class KnnOCR(ocr.OCR):
 
-def _labelize(labels):
-    return {k: i for i, k in enumerate(list(labels))}
-def _delabelize(labels):
-    return {i: k for i, k in enumerate(list(labels))}
+    def __init__(self, train_set=None, dump=None, load=None, glyphs=ocr.GLYPHS, verbose=False):
+        self.knn = self.__train(train_set, dump, load)
+        self.verbose=False
+        self.glyphs = glyphs
 
-def _unpackage(train_set):
-    data, labels = list(), list()
-    for (l, d) in train_set:
-        data.append(d)
-        labels.append(l)
-    return np.array(data), np.array(labels)
+    def read(self, input, k=5):
+        """Reads the number in the input image passed, k is the knn paramether"""
+        return self.__nearest(input, self.knn, k, self.glyphs, verbose=self.verbose)
 
-@common.showtime
-def train(train_set, size, dump=None, load=None):
-    """Trains a knn with the given train_set of samples.
-        size is the size of each sample
-        if dump is a file path (without estention) it'll save the trainset there
-        if load is a file path (without estention) it'll load the trainset from there
-            train_set will be ignored if these are defined
-            they can't be defined both as it makes no sense
-    returns the knn object
-    """
-    if dump is not None and load is not None:
-        raise ValueError("You can't both dump and load as it doesn't make sense")
+    def __unpackage(self, train_set):
+        data, labels = list(), list()
+        for (l, d) in train_set:
+            data.append(d)
+            labels.append(l)
+        return np.array(data), np.array(labels)
 
-    if load is not None:
-        with np.load("{}.npz".format(load)) as save:
-            data, labels = save["data"], save["labels"]
-    else:
-        data, labels = _unpackage(train_set)
+    @common.showtime
+    def __train(self, train_set=None, dump=None, load=None):
+        """Trains a knn with the given train_set of samples.
+            if dump is a file path (without estention) it'll save the trainset there
+            if load is a file path (without estention) it'll load the trainset from there
+                train_set will be ignored if these are defined
+        returns the knn object
+        """
 
-    data = data.reshape(-1, size[0]*size[1]).astype(np.float32)
+        if load is not None:
+            with np.load("{}.npz".format(load)) as save:
+                data, labels = save["data"], save["labels"]
+        else:
+            data, labels = self.__unpackage(train_set)
 
-    knn = cv2.ml.KNearest_create()
-    knn.train(data, cv2.ml.ROW_SAMPLE, labels)
+        size = reduce(lambda a, b: a*b, data[0].shape)
+        data = data.reshape(-1, size).astype(np.float32)
 
-    if dump is not None:
-        np.savez("{}.npz".format(dump), data=data, labels=labels)
-    
-    return knn
+        knn = cv2.ml.KNearest_create()
+        knn.train(data, cv2.ml.ROW_SAMPLE, labels)
 
-@common.showtime
-def nearest(input, knn, k=5, glyphs=GLYPHS, verbose=False):
-    """Given a knn object and an input mask will return the label of the mask for the curren knn training"""
-    samp = np.array(input).reshape(1, input.shape[0]*input.shape[1]).astype(np.float32)
-    print(samp.shape)
-    ret, res, neigh, dist = knn.findNearest(samp, k=k)
-
-    if verbose:
-        print("r:{}, res:{}, neigh:{}, dist:{}".format(ret, res, neigh, dist))
-
-    lbl = _delabelize(glyphs)[res[0][0]]
-
-    return lbl
-
-def get_train_set(size, glyphs=GLYPHS, verbose=False):
-    """Will generate a sized trainset"""
-    result = list()
-    lbl = _labelize(glyphs)
-    for i in range(size):
-        glyph = random.choice(glyphs) #TODO remove this random 
-        plate = generator.get_all_tables(glyph)[glyph]
-
-        mask, _ = extract.get_optimal_mask(plate)
-
-        result.append((lbl[glyph], mask))
-        if verbose:
-            print("{}/{}".format(i, size))
+        if dump is not None:
+            np.savez("{}.npz".format(dump), data=data, labels=labels)
         
-    return result, mask.shape
+        return knn
+
+    @common.showtime
+    def __nearest(self, input, knn, k, glyphs, verbose=False):
+        """Given a knn object and an input mask will return the label of the mask for the curren knn training"""
+        samp = np.array(input).reshape(1, input.shape[0]*input.shape[1]).astype(np.float32)
+
+        ret, res, neigh, dist = knn.findNearest(samp, k=k)
+
+        if verbose:
+            print("r:{}, res:{}, neigh:{}, dist:{}".format(ret, res, neigh, dist))
+
+        lbl = ocr.OCR.delabelize(glyphs)[res[0][0]]
+
+        return lbl
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        pass
 
 
 if __name__ == "__main__":
+    import generator
+    import extract
+    
     gen = False # if True will calculate a new trainset
+    size = 1 #size of the trainset
+    TOT = 30 #size of the testset
     if gen:
-        s, size = get_train_set(200, verbose=True)
+        size = 1
+        s = ocr.OCR.get_train_set(size, verbose=True)
+        l = None
+
+        assert len(s)==len(ocr.GLYPHS)*size, "Must generate the correct number of element ({}, {})".format(len(s), len(ocr.GLYPHS)*size)
     else:
         s = None
-        size = (1, 89590)
-
-    k = train(s, size, load="data_set")
+        l = "data_set"
     print("Trained")
     
-    res = 0
-    TOT = 30
-    for i in range(TOT):
-        c = str(i%10)
-        t, _ = extract.get_optimal_mask(generator.get_all_tables(c)[c])
+    with KnnOCR(load=l, train_set=s, verbose=True) as o:
+        assert o is not None, "A new object must be created"
+        res = 0
+        for i in range(TOT):
+            c = str(i%10)
+            t, _ = extract.get_optimal_mask(generator.get_all_tables(c)[c])
 
-        r = nearest(t, k)
-        print("{} {}".format(r, c))
-        if r == c:
-            res+=1
-    print("Accuracy: {:.2f} ({}/{})".format(res/TOT, res, TOT))
+            r = o.read(t)
+
+            assert r is not None, "ocr.read(...) must yield a result"
+            print("{} {}".format(r, c))
+            if r == c:
+                res+=1
+        print("Accuracy: {:.2f} ({}/{})".format(res/TOT, res, TOT))
