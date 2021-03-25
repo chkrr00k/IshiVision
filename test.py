1ae193eb4043f0bee7d460bf5345f3e1537b6161
chkrr00k <chkrr00k@cock.li>
2021-03-25 15:02:38 +0100

    Terminated main writing

--- test.py
+++ test.py
@@ -26,10 +26,11 @@ HELP_MESSAGE = """Help:
 -a, --accuracy <int>    Calculates the accuracy
 -c, --char <char>       Specify the char to test
 --gkt                   Enables gkt fixes for debian 10 and OpenCV 3.something
+--silent                Produce no output
 """
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "k:tl:d:vs:ha:c:", ["ocr", "train", "load", "dump", "verbose", "size", "help", "debug", "accuracy", "char", "gtk"])
+    opts, args = getopt.getopt(sys.argv[1:], "k:tl:d:vs:ha:c:", ["ocr", "train", "load", "dump", "verbose", "size", "help", "debug", "accuracy", "char", "gtk", "silent"])
 except getopt.GetoptError:
     print("Wrong argument")
     print(HELP_MESSAGE)
@@ -49,7 +50,8 @@ settings = {
         "v" : False,
         "db" : False,
         "a" : 0,
-        "c" : None
+        "c" : None,
+        "sil" : False
         }
 for opt, arg in opts:
     if opt in ("-k", "--ocr"):
@@ -76,6 +78,8 @@ for opt, arg in opts:
     elif opt in ("--gtk"):
         import gi
         gi.require_version("Gtk", "2.0")
+    elif opt in ("--silent") :
+        settings["sil"] = True
 
 if settings["l"] and settings["d"]:
     print("Setting -l and -d makes sense only if you want to copy your trainset, be aware!")
@@ -98,11 +102,13 @@ accuracy = settings["a"]
 character = settings["c"]
 size = settings["s"]
 train = size if settings["t"] else None
+silent = settings["sil"]
 
 if debug:
     print("Debug mode: ON")
     print("Settings: {}".format(settings))
     common.debug = True
+    verbose = True
 
 if accuracy > 0:
     hits = 0
@@ -113,7 +119,8 @@ if accuracy > 0:
             r = o.read(img)
             if r == c:
                 hits += 1
-    print("Accuracy: {:.2f} ({}/{})".format(hits/accuracy, hits, accuracy))
+    if not silent:
+        print("Accuracy: {:.2f} ({}/{})".format(hits/accuracy, hits, accuracy))
 else:
     character = character if character is not None else random.choice("1234567890")
     if debug:
@@ -123,5 +130,6 @@ else:
     with ocr_class(train_set=train, dump=dump, load=load, verbose=verbose) as o:
         r = o.read(img)
 
-    print("Found '{}' {}".format(r, "({} was expected)".format(character) if r != character else ""))
+    if not silent:
+        print("Found '{}' {}".format(r, "({} was expected)".format(character) if r != character else ""))
 
