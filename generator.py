25854c09932de653e5394a85560636549e3a11d7
chkrr00k <chkrr00k@cock.li>
2021-04-06 12:51:21 +0200

    Added commands to call the generator.py from terminal

--- generator.py
+++ generator.py
@@ -65,14 +65,69 @@ def ishihara(base, size, base_bg, bg, n_circles, c_on, c_off):
 
     return result
 #c_on=[_color(0xF9BB82), _color(0xEBA170), _color(0xFCCD84)], c_off=[_color(0x9CA594), _color(0xACB4A5), _color(0xBBB964), _color(0xD7DAAA), _color(0xE5D57D), _color(0XD1D6AF)]
-def get_all_tables(glyphs, heigh=200, bezel=40, thic=20, bg=(255,255,255), n_circles=550, c_on=[_color(0xF9BB82),  _color(0xFCCD84), _color(0xEBA170)], c_off=[_color(0x9CA594), _color(0xACB4A5), _color(0xa3a260),_color(0xBBB964), _color(0xD7DAAA), _color(0xD1D6AF)], fonts=[cv2.FONT_HERSHEY_SCRIPT_COMPLEX]):
+C_ON = [_color(0xF9BB82),  _color(0xFCCD84), _color(0xEBA170)]
+C_OFF = [_color(0x9CA594), _color(0xACB4A5), _color(0xa3a260),_color(0xBBB964), _color(0xD7DAAA), _color(0xD1D6AF)]
+def get_all_tables(glyphs, heigh=200, bezel=40, thic=20, bg=(255,255,255), n_circles=550, c_on=C_ON, c_off=C_OFF, fonts=[cv2.FONT_HERSHEY_SCRIPT_COMPLEX]):
     """Returns a dictionary with ishihara'd glyphs, parameters may be applied"""
     return {k: ishihara(v, v.shape, 0, bg, n_circles, c_on, c_off) for k, v in matcher.get_all_glyphs_refs(glyphs, heigh, bezel, thic, fonts).items()} #FIXME inapropriate use of dict
 
 
 if __name__ == "__main__":
-    for k, v in get_all_tables("1234567890", c_off=[_color(0xBAB8AF)], c_on=[_color(0x4B4B4B), _color(0x747474)]).items():
-        cv2.imshow(k, v)
+    import getopt
+    import sys
+    import re
+    HELP_MESSAGE= """Help:
+-h, --help                  Displays this help
+-g, --glyph <glyph>         Render the given glyph
+-bg <comma separated hex>   Uses the given colors for the background
+                            (e.g "0x000000, 0xAAAAAA")
+-fg <comma separated hex>   Uses the given colors for the foreground
+-v, --verbose               Verbose prints
+<file name>                 Will be saved as a .PNG regardless
+    """
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "vg:h", ["glyph=", "bg=", "fg=","verbose", "help"])
+    except getopt.GetoptError:
+        print("Wrong argument")
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    settings = {
+            "g": random.choice("1234567890"),
+            "v": False,
+            "bg": C_OFF,
+            "fg": C_ON
+            }
+    def get_colors(input):
+        if settings["v"]:
+            print("Inputed: {}".format(re.split("[,;]\s*", input)))
+        return [_color(int(i, 0)) for i in re.split("[,;]\s*", input)]
+
+    for opt, arg in opts:
+        if opt in ("-v", "--verbose"):
+            settings["v"] = True
+        elif opt in ("--fg"):
+            settings["fg"] = get_colors(arg)
+        elif opt in ("--bg"):
+            settings["bg"] = get_colors(arg)
+        elif opt in ("-g", "--glyph"):
+            settings["g"] = str(arg)
+        elif opt in ("-h", "--help"):
+            print(HELP_MESSAGE)
+            sys.exit(1)
+    
+    filename = re.split("\.", args[0])[0] if len(args) > 0 else "base"
+    if settings["v"]:
+        print("Saving on {}.png".format(filename))
+        print("Rendering {}".format(settings["g"]))
+
+    base = matcher.render_glyph(settings["g"], 200)
+    img = ishihara(base, base.shape, 0, (255,255,255), 500, settings["fg"], settings["bg"])
+    cv2.imwrite("{}.png".format(filename), img)
+
+#    for k, v in get_all_tables("1234567890", c_off=[_color(0xBAB8AF)], c_on=[_color(0x4B4B4B), _color(0x747474)]).items():
+    
+    
+#    cv2.imshow("img", img)
+#    cv2.imshow("base", base)
+
+ #   cv2.waitKey(0)
+ #   cv2.destroyAllWindows()
